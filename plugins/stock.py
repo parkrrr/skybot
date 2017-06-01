e0f217363b234756898e15cf83245ad262e44a6d
Parker Smith <parkrrr@users.noreply.github.com>
2017-06-01 13:18:17 -0400

    Convert stock to use Google's finance data

--- plugins/stock.py
+++ plugins/stock.py
@@ -1,46 +1,42 @@
+import json, urllib2
 from util import hook, http
-
 
 @hook.command
 def stock(inp):
     '''.stock <symbol> -- gets stock information'''
 
-    url = ('http://query.yahooapis.com/v1/public/yql?format=json&'
-           'env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
-
-    parsed = http.get_json(url, q='select * from yahoo.finance.quotes '
-                           'where symbol in ("%s")' % inp)  # heh, SQLI
-
-    quote = parsed['query']['results']['quote']
-
-    # if we dont get a company name back, the symbol doesn't match a company
-    if quote['Change'] is None:
-        return "unknown ticker symbol %s" % inp
-
-    price = float(quote['LastTradePriceOnly'])
-    change = float(quote['Change'])
-    if quote['Open'] and quote['Bid'] and quote['Ask']:
-        open_price = float(quote['Open'])
-        bid = float(quote['Bid'])
-        ask = float(quote['Ask'])
-        if price < bid:
-            price = bid
-        elif price > ask:
-            price = ask
-        change = price - open_price
-        quote['LastTradePriceOnly'] = "%.2f" % price
+    symbols = inp.split(',')
+    # who needs more than 3 symbols
+    url = ('http://finance.google.com/finance/info?client=ig&q=%s' % ','.join(symbols[:3]))
+    
+    try:
+        raw = http.get(url)
+    except urllib2.HTTPError as err:
+        if err.code == 400:
+            return "unknown ticker symbol %s" % inp
+        else:
+            return "error %s while retrieving data" % err.code
+        
+    # remove the comment stuff
+    fixed = raw.replace('//', '')
+    parsed = json.loads(fixed)
+    
+    s = []
+    for q in parsed:
+        quote = parsed[0]
+        change = float(q['c'])
+        
+        quote['Name'] = q['t']
+        quote['ChangePercentage'] = q['cp']
+        quote['LastTradePriceOnly'] = "%.2f" % float(q['l'])
         quote['Change'] = ("+%.2f" % change) if change >= 0 else change
 
-    if change < 0:
-        quote['color'] = "5"
-    else:
-        quote['color'] = "3"
+        if change < 0:
+            quote['color'] = "5"
+        else:
+            quote['color'] = "3"   
 
-    quote['PercentChange'] = 100 * change / (price - change)
+        ret = "%(Name)s - %(LastTradePriceOnly)s \x03%(color)s%(Change)s (%(ChangePercentage)s%%)\x03" % quote
+        s.append(ret)
 
-    ret = "%(Name)s - %(LastTradePriceOnly)s "                   \
-          "\x03%(color)s%(Change)s (%(PercentChange).2f%%)\x03 "        \
-          "Day Range: %(DaysRange)s " \
-          "MCAP: %(MarketCapitalization)s" % quote
-
-    return ret
+    return ', '.join(s)
